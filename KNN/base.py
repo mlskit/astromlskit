681aa31f06da5c1173172c9700ba3bf302f8ab9e
vishnumani2009 <vishnumani.2009@gmail.com>
2015-02-06 20:55:44 +0530

    Removed test CSV Files

--- KNN/base.py
+++ KNN/base.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2011, Luis Pedro Coelho <luis@luispedro.org>
-# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
-# License: MIT. See COPYING.MIT file in the milk distribution
 
 from __future__ import division
 
